66d086b06722c4162418305417d135e3174642c9
Bimo231 <arga.bimo.09@gmail.com>
2023-10-29 00:34:43 +0700

    add (html) : mengubah warna

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const loginText = document.querySelector(".title-text .login");
+giconst loginText = document.querySelector(".title-text .login");
       const loginForm = document.querySelector("form.login");
       const loginBtn = document.querySelector("label.login");
       const signupBtn = document.querySelector("label.signup");
