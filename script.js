4186ca290b9b8a1128005a34aadacfaeded7c9d0
Bimo231 <arga.bimo.09@gmail.com>
2023-10-29 14:56:10 +0700

    add (html) tambah js untuk dashboard

--- script.js
+++ script.js
@@ -15,3 +15,7 @@ signupLink.onclick = () => {
   signupBtn.click();
   return false;
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Halaman dimuat sepenuhnya.');
+});
